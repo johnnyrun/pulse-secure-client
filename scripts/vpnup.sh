525aae6cf2c4a2996275ff845f4280efeff3e189
Gianni Carabelli <gianni.carabelli@skytv.it>
2021-01-11 14:51:18 +0000

    pulseaudio

--- scripts/vpnup.sh
+++ scripts/vpnup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 test -n "$IMAGE" || export IMAGE=johnnyrun/pulse-secure-client
-docker ps -a |grep pulse-client || docker create --privileged --name pulse-client --device /dev/net/tun --cap-add net_admin --cap-add sys_admin --volume /tmp/.X11-unix:/tmp/.X11-unix --env DISPLAY  -v ~/.pulse:/data  $IMAGE
+docker ps -a |grep pulse-client || docker create --privileged --name pulse-client --device /dev/net/tun --cap-add net_admin --cap-add sys_admin --volume /tmp/.X11-unix:/tmp/.X11-unix --env DISPLAY  -v ~/.pulse-secure:/data  $IMAGE
 CONTAINERIP=$(docker inspect pulse-client | jq '.[].NetworkSettings.Networks.bridge.IPAddress' -r)
 xhost +$CONTAINERIP
 docker ps  |grep pulse-client || docker start pulse-client
